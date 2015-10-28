3815b03ed1b5ecfc35f23e70cb2fc26a3440f95c
Jemd13 <jemd311@gmail.com>
2015-10-28 19:07:07 -0430

    Links funcionales (la mayoria) y fondos

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,13 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def agrupacion
+  end
+
+  def preparadores
+  end
+
+  def admision
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root 'pages#home'
-
+  get  'agrupacion'   => 'pages#agrupacion'
+  get  'servicios' 	  => 'pages#servicios'
+  get  'preparadores' => 'pages#preparadores'
+  get  'admision'	  => 'pages#admision'
 end
